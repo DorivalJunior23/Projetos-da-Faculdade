9478ca0b5fb2ef4afaf68a5c92c60f14e40a588e
Dorival Junior <dorivaljunior23@outlook.com>
2021-10-26 15:20:11 -0400

    mutiplica a matriz por ela mesma

---
+++ main (6).c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int
+main ()
+{
+  int i, j, x, y = 0, k, z = 0, f = 0, l = 0;
+  scanf ("%i", &x);
+  double matriz[x][x], v[x], c[x], multi[x], soma = 0;
+  z = x * x;
+  for (i = 0; i < x; i++)
+    {
+      for (j = 0; j < x; j++)
+	{
+	  scanf ("%lf", &matriz[i][j]);
+	}
+    }
+
+  do
+    {
+      for (k = 0; k < x; k++)
+	{
+	  for (j = 0, i = l; j < x; j++)
+	    {
+	      c[j] = matriz[i][j];
+
+	    }
+
+	  for (i = 0, j = y; i < x; i++)
+	    {
+	      v[i] = matriz[i][j];
+
+	    }
+
+	  y++;
+	  if (y == x)
+	    {
+	      y = 0;
+	    }
+	  for (i = 0; i < x; i++)
+	    {
+	      multi[i] = c[i] * v[i];
+	    }
+	  for (i = 0; i < x; i++)
+	    {
+	      soma = multi[i] + soma;
+	    }
+	  printf ("%.1lf ", soma);
+	  f++;
+	  soma = 0;
+	}
+      l++;
+      printf ("\n");
+    }
+  while (f < z);
+}
