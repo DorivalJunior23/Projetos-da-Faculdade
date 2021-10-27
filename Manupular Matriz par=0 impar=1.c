9cd1860e5e3932aa9bcfe6fa79a0d7718d2462d9
Dorival Junior <dorivaljunior23@outlook.com>
2021-10-27 16:00:40 -0400

    Manipular matriz

--- Manupular Matriz par=0 impar=1.c
+++ Manupular Matriz par=0 impar=1.c
@@ -1,16 +1,8 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 int leMatriz(int n){
     int i, j,matriz[n][n];
-     
+
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
             scanf("%i",&matriz[i][j]);
@@ -52,5 +44,5 @@ int main()
     printaMatriz(a);
     parImpar(a);
     printaMatriz(a);
-   
+
 }
