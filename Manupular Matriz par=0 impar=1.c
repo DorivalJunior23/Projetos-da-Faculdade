ed9ed04f773ead6e08135571d6e5e11edf7fe6f8
Dorival Junior <dorivaljunior23@outlook.com>
2021-10-27 15:52:58 -0400

    Manipolar matrizes

---
+++ Manupular Matriz par=0 impar=1.c
@@ -0,0 +1,56 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int leMatriz(int n){
+    int i, j,matriz[n][n];
+     
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            scanf("%i",&matriz[i][j]);
+        }
+    }
+}
+int printaMatriz(int n){
+    int i,j,matriz[n][n];
+    printf("Matriz:\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf("%i    ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    printf("-------------------\n");
+}
+int parImpar(int n){
+    int i,j,aux=0,matriz[n][n];
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(matriz[i][j]%2==0){
+                matriz[i][j]=0;
+            }else{
+                matriz[i][j]=1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int a,z;
+    printf("Informe o tamanho da matriz: ");
+    scanf("%i", &a);
+    z=a*a;
+    printf("Informe %i valores para a matriz: \n", z);
+    leMatriz(a);
+    printaMatriz(a);
+    parImpar(a);
+    printaMatriz(a);
+   
+}
